926bf52e27378d2bb9faa6f7ca655584672598ec
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2018-03-24 22:41:18 +0800

    借閱人不能查別人的demo測試

--- src/library/system/Main.java
+++ src/library/system/Main.java
@@ -1,8 +1,19 @@
 package library.system;
+
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
         
+//        Staff userA = new Staff("userA");
+//        userA.findChecked("userB");
+//        
         
+        
+        
+        
+        Borrower userB = new Borrower("userB");
+        userB.findChecked("userA");
     }
     
 }
--- src/model/User.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import bean.Book;
-import java.util.List;
-
-public abstract class User {
-    public abstract List<Book> findChecked(String userA,String userB);
-    //員工查看這個人借走了哪些書，或使用者查自己借了哪些書
-}
