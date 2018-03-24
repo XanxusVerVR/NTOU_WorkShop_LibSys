0ed13031e5446e5f149d09b5a8504ada005da351
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2018-03-24 23:57:02 +0800

    加入新的book跟lib

--- src/bean/Book.java
+++ src/bean/Book.java
@@ -1,41 +1,49 @@
 package bean;
 
 public class Book {
-
-    private int bookId;
+private int bookId;
     private String author;
     private String subject;
     private boolean isCheck;
+    private String borrower;
 
-    public void setBookId(int id) {
+    public void setBookId(int id){
         this.bookId = id;
     }
 
-    public int getBookId() {
+    public int getBookId(){
         return this.bookId;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(String author){
         this.author = author;
     }
 
-    public String getAuthor() {
+    public String getAuthor(){
         return this.author;
     }
 
-    public void setSubject(String subject) {
+    public void setSubject(String subject){
         this.subject = subject;
     }
 
-    public String getSubject() {
+    public String getSubject(){
         return subject;
     }
 
-    public void setIsCheck(boolean isCheck) {
+    public void setIsCheck(boolean isCheck){
         this.isCheck = isCheck;
     }
 
-    public boolean getIsCheck() {
+    public boolean getIsCheck(){
         return isCheck;
+    }
+
+    public void setBorrower(String borrower){
+        this.borrower = borrower;
+    }
+
+    public String getBorrower(){
+        return this.borrower;
     }
 }
---
+++ src/model/LibraryRepository.java
@@ -0,0 +1,132 @@
+package model;
+
+import bean.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryRepository {
+    private static List<Book> bookList = new ArrayList<Book>();;
+    private static int currentId;
+    //如果false代表lib已存在相同subject
+    public static boolean addBookToList(Book book){
+        boolean existbook = false;
+        boolean res = false;
+        if(!bookList.isEmpty())
+            existbook = existBook(book.getSubject());
+
+        if(!existbook){
+            book.setBookId(currentId++);
+            bookList.add(book);
+            res = true;
+        }
+        return res;
+    }
+
+    //如果為false則代表不存在該book，problem:如果book被借走還可以remove嗎?
+    public static boolean romoveBookByList(Book book){
+        boolean existbook = existBook(book.getSubject());
+        boolean res = false;
+        if(existbook) {
+            bookList.remove(book);
+        }
+        return res;
+    }
+
+    //如果book以借出或不存在則false
+    public static boolean checkoutBook(Book book){
+        boolean res = false;
+        int bookid = book.getBookId();
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getBookId() == bookid) {
+                if(!bookList.get(i).getIsCheck()){
+                    bookList.get(i).setIsCheck(true);
+                    res = true;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static boolean returnBook(Book book){
+        boolean res = false;
+        int bookid = book.getBookId();
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getBookId() == bookid) {
+                if(!bookList.get(i).getIsCheck()){
+                    bookList.get(i).setIsCheck(false);
+                    res = true;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static Book findBookById(int id){
+        Book book = null;
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getBookId() == id)
+                book = bookList.get(i);
+        }
+        return book;
+    }
+
+    //如果回傳null則代表不存在該作者
+    public static List<Book> findBookByAuthor(String author){
+        List<Book> list = new ArrayList<Book>();
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getAuthor() == author)
+                list.add(bookList.get(i));
+        }
+        return list;
+    }
+
+    public static List<Book> findBookBySubject(String subject){
+        List<Book> list = new ArrayList<Book>();
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getSubject() == subject)
+                list.add(bookList.get(i));
+        }
+        return list;
+    }
+
+    public static List<Book> findBookByBorrower(String borrower){
+        List<Book> list = new ArrayList<Book>();
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getBorrower() == borrower)
+                list.add(bookList.get(i));
+        }
+        return list;
+    }
+    //null代表沒有被借出
+    public static String findBorrower(int id){
+        Book book = null;
+        if(isCheckedOut(id)){
+            book = findBookById(id);
+        }
+        return book.getBorrower();
+    }
+
+    public static int getCurrentId(){
+        return currentId;
+    }
+
+    private static boolean existBook(String subject){
+        boolean res = false;
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getSubject() == subject)
+                res = true;
+        }
+        return res;
+    }
+
+    private static boolean isCheckedOut(int id){
+        boolean res = false;
+        for(int i = 0; i < bookList.size();i++){
+            if(bookList.get(i).getBookId() == id) {
+                if(bookList.get(i).getIsCheck())
+                    res = true;
+            }
+        }
+        return res;
+    }
+}
